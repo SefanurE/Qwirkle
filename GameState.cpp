5cf7327798fad7a97cff905917b4f6768ff8b70c
Sivan Krispin s3856571 <s3856571@student.rmit.edu.au>
2021-05-02 11:48:23 +1000

    Add GameState class

---
+++ GameState.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include "GameState.h"
+
+GameState::GameState() {
+
+}
+
+GameState::~GameState(){
+}
---
+++ GameState.h
@@ -0,0 +1,23 @@
+#ifndef COSC_ASSIGN_TWO_GS
+#define COSC_ASSIGN_TWO_GS
+#include <iostream>
+#include <vector>
+#include <string>
+
+
+class GameState{
+public:
+
+    GameState(int w, int h);
+    ~GameState();
+
+
+
+private:
+
+
+
+
+};
+
+#endif //COSC_ASSIGN_TWO_GS

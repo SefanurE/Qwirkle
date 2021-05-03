05a9861d3d40c46c0748407c63cdb29ba7303416
[Ewan Breakey s3845382] <s3845382@student.rmit.edu.au>
2021-05-03 13:41:12 +1000

    Update GameState header to reflect model

--- GameState.h
+++ GameState.h
@@ -5,16 +5,18 @@
 #include <string>
 #include "Player.h"
 #include "TileBag.h"
+#include "Board.h"
 
-class GameState{
+class GameState {
   public:
-
     GameState(Player* players);
     ~GameState();
     void InitialHand(LinkedList* hand);
 
   private:
-
+    Player* players;
+    Board* board;
+    TileBag bag;
 };
 
 #endif //COSC_ASSIGN_TWO_GS

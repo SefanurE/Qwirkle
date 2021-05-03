9a4d688682438c610662d0861c632b1aeaef2669
[Ewan Breakey s3845382] <s3845382@student.rmit.edu.au>
2021-05-03 13:39:07 +1000

    Remove test code from main

--- qwirkle.cpp
+++ qwirkle.cpp
@@ -6,9 +6,5 @@
 #define EXIT_SUCCESS    0
 
 int main(void) {
-  TileBag* bag = new TileBag();
-  std::cout << bag->getList()->toString() << std::endl;
-  bag->shuffle();
-  std::cout << bag->getList()->toString() << std::endl;
   return EXIT_SUCCESS;
 }
